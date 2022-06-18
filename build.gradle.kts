83aa1b4532601273e9626909a53b2fbc9c72b0fd
alexw5335 <53756287+alexw5335@users.noreply.github.com>
2022-06-18 12:42:50 +1000

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+
+
+plugins {
+    kotlin("jvm") version "1.7.0"
+}
+
+
+
+group = "placeholder"
+version = "1.0"
+
+
+
+allprojects {
+	group = project.group
+	version = project.version
+
+	repositories {
+		mavenCentral()
+	}
+
+	apply {
+		tasks.withType<KotlinCompile> {
+			kotlinOptions.jvmTarget = "17"
+		}
+
+		plugin("org.jetbrains.kotlin.jvm")
+	}
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+rootProject.name = "kotlin-projects"
+
+include(":module-core")
