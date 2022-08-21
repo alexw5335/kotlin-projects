57d003944f93878d21f84667135a03c0c10f1360
alexw5335 <53756287+alexw5335@users.noreply.github.com>
2022-08-21 18:14:53 +1000

    .

--- module-rome2/src/main/kotlin/rome2/Rome2Main.kt
+++ module-rome2/src/main/kotlin/rome2/Rome2Main.kt
@@ -1,17 +1,18 @@
 package rome2
 
 import core.Core
-import java.io.File
 
 
 
 fun main() {
-	for(dir in File("db").listFiles()!!) {
-		val file = dir.listFiles()!![0]
-		val firstLine = file.readLines()[0]
-		if(file.name == "unit_shield_types")
-			println(firstLine)
-	}
+	modifyArmours()
+	modifyShields()
+	modifyWeapons()
+	modifyUnits()
+	applyMods("RAI", Tables.ARMOURS, armours.values)
+	applyMods("RAI", Tables.SHIELDS, shields.values)
+	applyMods("RAI", Tables.WEAPONS, weapons.values)
+	applyMods("RAI", Tables.LAND_UNITS, Tables.MAIN_UNITS, units.values)
 }
 
 
@@ -22,134 +23,26 @@ Modifications
 
 
 
-fun applyBuildingMods(mods: List<BuildingMod>) {
-
+fun applyMods(prefix: String, table: Table, mods: Collection<Rome2Object>) {
+	val lines = ArrayList<String>()
+	lines.add(table.keys)
+	for(mod in mods) lines.add(mod.assembleLine)
+	Core.writeLines(table.outputPath(prefix), lines)
 }
 
 
 
-fun applyArmourMods(mods: List<ArmourMod>) {
-	val lines = Core.readResourceLines("/unit_armour_types.txt")
-	val outputLines = ArrayList<String>()
-
-	outputLines.add("key\tarmour_value\tbonus_vs_missiles\tweak_vs_missiles\taudio_material")
-	outputLines.add("#unit_armour_types_tables;1;db/unit_armour_types_tables/RAI_unit_armour_types\t")
-
+fun applyMods(prefix: String, table1: Table, table2: Table, mods: Collection<Rome2Object2>) {
+	val lines1 = ArrayList<String>()
+	val lines2 = ArrayList<String>()
+	lines1.add(table1.keys)
+	lines2.add(table2.keys)
 	for(mod in mods) {
-		val parts = lines.first { it.substringBefore('\t') == mod.name }.split('\t').toMutableList()
-		mod.armour?.let { parts[1] = it.toString() }
-		outputLines.add(parts.joinToString("\t"))
+		lines1.add(mod.assembleLine1)
+		lines2.add(mod.assembleLine2)
 	}
-
-	Core.writeLines("db/unit_armour_types_tables/RAI_unit_armour_types.tsv", outputLines)
-}
-
-
-
-fun applyShieldMods(mods: List<ShieldMod>) {
-	val lines = Core.readResourceLines("/unit_shield_types.txt")
-	val outputLines = ArrayList<String>()
-
-	outputLines.add("key\tshield_defence_value\tshield_armour_value\taudio_material\tmissile_block_chance")
-	outputLines.add("#unit_shield_types_tables;3;db/unit_shield_types_tables/RAI_unit_shield_types")
-
-	for(mod in mods) {
-		val parts = lines.first { it.substringBefore('\t') == mod.name }.split('\t').toMutableList()
-		mod.defence?.let { parts[1] = it.toString() }
-		mod.armour?.let { parts[2] = it.toString() }
-		mod.missileBlockChance?.let { parts[4] = it.toString() }
-		outputLines.add(parts.joinToString("\t"))
-	}
-
-	Core.writeLines("db/unit_shield_types_tables/RAI_unit_shield_types.tsv", outputLines)
-}
-
-
-
-fun applyWeaponMods(mods: List<WeaponMod>) {
-	val lines = Core.readResourceLines("/melee_weapons.txt")
-	val outputLines = ArrayList<String>()
-
-	outputLines.add("key\tarmour_penetrating\tarmour_piercing\tbonus_v_cavalry\tbonus_v_elephants\tbonus_v_infantry\tdamage\tap_damage\tfirst_strike\tshield_piercing\tweapon_length\tmelee_weapon_type\taudio_material")
-	outputLines.add("#melee_weapons_tables;6;db/melee_weapons_tables/RAI_melee_weapons")
-
-	for(mod in mods) {
-		val parts = lines.first { it.substringBefore('\t') == mod.name }.split('\t').toMutableList()
-		mod.damage?.let { parts[6] = it.toString() }
-		mod.apDamage?.let { parts[7] = it.toString() }
-		mod.cavalryBonus?.let { parts[3] = it.toString() }
-		mod.largeBonus?.let { parts[4] = it.toString() }
-		mod.infantryBonus?.let { parts[5] = it.toString() }
-		outputLines.add(parts.joinToString("\t"))
-	}
-
-	Core.writeLines("db/melee_weapons_tables/RAI_melee_weapons.tsv", outputLines)
-}
-
-
-
-
-fun applyUnitMods(mods: List<UnitMod>) {
-	applyUnitMods1(mods)
-	applyUnitMods2(mods)
-}
-
-
-
-fun applyUnitMods1(mods: List<UnitMod>) {
-	val lines = Core.readResourceLines("/land_units.txt").filter { it.startsWith("Rom_") }
-	val outputLines = ArrayList<String>()
-
-	outputLines.add("key\taccuracy\tammo\tarmour\tcampaign_action_points\tcategory\tcharge_bonus\tclass\tdismounted_charge_bonus\tdismounted_melee_attack\tdismounted_melee_defense\thistorical_description_text\tman_animation\tman_entity\tmelee_attack\tmelee_defence\tmorale\tbonus_hit_points\tmount\tnum_animals\tanimal\tnum_mounts\tprimary_melee_weapon\tprimary_missile_weapon\trank_depth\tshield\tshort_description_text\tspacing\tstrengths_&_weaknesses_text\tsupports_first_person\ttraining_level\tnum_guns\tofficers\tarticulated_record\tengine\tis_male\tvisibility_spotting_range_min\tvisibility_spotting_range_max\tability_global_recharge\tattribute_group\tspot_dist_tree\tspot_dist_scrub\tchariot\tnum_chariots\treload\tloose_spacing\tspotting_and_hiding\tselection_vo\tselected_vo_secondary\tselected_vo_tertiary\thiding_scalar")
-	outputLines.add("#land_units_tables;27;db/land_units_tables/RAI_land_units")
-
-	for(mod in mods) {
-		val parts = lines.first { it.substringBefore('\t') == mod.unit.name }.split('\t').toMutableList()
-		mod.attack?.let { parts[14] = it.toString(); parts[9] = it.toString() }
-		mod.defence?.let { parts[15] = it.toString(); parts[10] = it.toString() }
-		mod.morale?.let { parts[16] = it.toString() }
-		mod.bonusHp?.let { parts[17] = it.toString() }
-		mod.charge?.let { parts[6] = it.toString(); parts[8] = it.toString() }
-		mod.armour?.let { parts[3] = it }
-		mod.weapon?.let { parts[22] = it }
-		mod.shield?.let { parts[25] = it }
-		outputLines.add(parts.joinToString("\t"))
-	}
-
-	Core.writeLines("db/land_units_tables/RAI_land_units.tsv", outputLines)
-}
-
-
-
-/*private fun<T> applyMods(input: String, columns: List<String>, block: (List<String>) ->) {
-	val lines = Core.readResourceLines("/$input.txt")
-	val outputLines = ArrayList<String>()
-
-	outputLines.add(columns.joinToString("\t"))
-	outputLines.add("#${input}_tables;${columns.size};db/${input}_tables/RAI_$input")
-
-	for(mod in mods) outputLines.add(block(mod))
-
-	Core.writeLines("db/${input}_tables/RAI_$input.tsv", outputLines)
-}*/
-
-
-
-fun applyUnitMods2(mods: List<UnitMod>) {
-	val lines = Core.readResourceLines("/main_units.txt").filter { it.startsWith("Rom_") }
-	val outputLines = ArrayList<String>()
-
-	outputLines.add("unit\tadditional_building_requirement\tcampaign_cap\tcaste\tcreate_time\tis_naval\tland_unit\tnum_men\tmultiplayer_cap\tmultiplayer_cost\tnaval_unit\tnum_ships\tmin_men_per_ship\tmax_men_per_ship\tprestige\trecruitment_cost\trecruitment_movie\treligion_requirement\tupkeep_cost\tweight\tcampaign_total_cap\tresource_requirement\tworld_leader_only\tcan_trade\tspecial_edition_mask\tunique_index\tin_encyclopedia\tregion_unit_resource_requirement\taudio_language\taudio_vo_actor_group")
-	outputLines.add("#main_units_tables;14;db/main_units_tables/RAI_main_units")
-
-	for(mod in mods) {
-		val parts = lines.first { it.substringBefore('\t') == mod.unit.name}.split('\t').toMutableList()
-		mod.cost?.let { parts[15] = it.toString(); parts[9] = it.toString() }
-		mod.upkeep?.let { parts[18] = it.toString() }
-		outputLines.add(parts.joinToString("\t"))
-	}
-
-	Core.writeLines("db/main_units_tables/RAI_main_units.tsv", outputLines)
+	Core.writeLines(table1.outputPath(prefix), lines1)
+	Core.writeLines(table2.outputPath(prefix), lines2)
 }
 
 
@@ -160,70 +53,25 @@ Info
 
 
 
-private fun<T> map(table: String, block: (List<String>) -> T?) = buildList {
-	for(l in Core.readResourceLines("/$table.txt"))
-		block(l.split('\t'))?.let(::add)
-}
+private fun tableLines(table: Table) = Core.readResourceLines("/${table.name}.txt")
 
 
 
-private fun String.toBoolean() = this == "1" || this == "true"
+val armours = tableLines(Tables.ARMOURS).map(::Armour).associateBy { it.name }
+
+val shields = tableLines(Tables.SHIELDS).map(::Shield).associateBy { it.name }
+
+val weapons = tableLines(Tables.WEAPONS).map(::Weapon).associateBy { it.name }
+
+val unitLines1 = tableLines(Tables.LAND_UNITS)
+
+val unitLines2 = tableLines(Tables.MAIN_UNITS).associateBy { it.substringBefore('\t') }
+
+val units = unitLines1.filter { unitLines2.contains(it.substringBefore('\t')) }.map { Unit(it, unitLines2[it.substringBefore('\t')] ?: error(it)) }.associateBy { it.name }
 
 
 
-val armours = map("unit_armour_types") {
-	Armour(it[0], it[1].toInt(), it[2].toInt(), it[3].toBoolean())
-}.associateBy { it.name }
-
-
-
-val shields = map("unit_shield_types") {
-	Shield(
-		name               = it[0],
-		defence            = it[1].toInt(),
-		armour             = it[2].toInt(),
-		missileBlockChance = it[4].toInt())
-}.associateBy { it.name }
-
-
-
-val weapons = map("melee_weapons") {
-	Weapon(
-		name             = it[0],
-		damage           = it[6].toInt(),
-		apDamage         = it[7].toInt(),
-		cavalryBonus     = it[3].toInt(),
-		largeBonus       = it[4].toInt(),
-		infantryBonus    = it[5].toInt(),
-		isArmourPiercing = it[2].toBoolean())
-}.associateBy { it.name }
-
-
-
-val unitInfos = map("main_units") {
-	UnitInfo(it[0], it[15].toInt(), it[18].toInt())
-}.associateBy { it.name }
-
-
-
-val units = map("land_units") {
-	Unit(
-		name    = it[0],
-		attack  = it[14].toInt(),
-		defence = it[15].toInt(),
-		morale  = it[16].toInt(),
-		bonusHp = it[17].toInt(),
-		charge  = it[6].toInt(),
-		armour  = armours[it[3]]!!,
-		weapon  = weapons[it[22]]!!,
-		shield  = shields[it[25]]!!,
-		cost    = unitInfos[it[0]]?.cost ?: -1,
-		upkeep  = unitInfos[it[0]]?.upkeep ?: -1
-	)
-}.filter { it.name.startsWith("Rom_") }.associateBy { it.name }
-
-
-
+/*
 val buildingInfos = map("building_levels") {
 	BuildingInfo(
 		name      = it[0],
@@ -256,6 +104,7 @@ val buildingEffects = HashMap<String, ArrayList<BuildingEffect>>().also { map ->
 val buildings = buildingInfos.map {
 	Building(it.name, it.level, it.buildTime, it.cost, buildingEffects[it.name] ?: emptyList())
 }.associateBy { it.name }
+*/
 
 
 
--- module-rome2/src/main/kotlin/rome2/Types.kt
+++ module-rome2/src/main/kotlin/rome2/Types.kt
@@ -4,6 +4,80 @@ import kotlin.reflect.KProperty
 
 
 
+/*
+Utils
+ */
+
+
+
+class BooleanProperty(private val parts: MutableList<String>, private val index: Int) {
+	private var value = parts[index] == "1" || parts[index] == "true"
+	operator fun getValue(thisRef: Any?, property: KProperty<*>) = value
+	operator fun setValue(thisRef: Any?, property: KProperty<*>, value: Boolean) {
+		parts[index] = "1"
+		this.value = value
+	}
+}
+
+
+
+class IntProperty(private val parts: MutableList<String>, private val index: Int) {
+	private var value = parts[index].toInt()
+	operator fun getValue(thisRef: Any?, property: KProperty<*>) = value
+	operator fun setValue(thisRef: Any?, property: KProperty<*>, value: Int) {
+		parts[index] = value.toString()
+		this.value = value
+	}
+}
+
+
+
+class StringProperty(private val parts: MutableList<String>, private val index: Int) {
+	operator fun getValue(thisRef: Any?, property: KProperty<*>) = parts[index]
+	operator fun setValue(thisRef: Any?, property: KProperty<*>, value: String) {
+		parts[index] = value
+	}
+}
+
+
+
+class ObjectProperty<T>(private val parts: MutableList<String>, private val index: Int, val getter: (String) -> T, val setter: (T) -> String) {
+	private var value = getter(parts[index])
+	operator fun getValue(thisRef: Any?, property: KProperty<*>) = value
+	operator fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
+		parts[index] = setter(value)
+		this.value = value
+	}
+}
+
+
+
+abstract class Rome2Object(line: String) {
+	protected val parts = line.split('\t').toMutableList()
+	val assembleLine get() = parts.joinToString("\t")
+	protected fun IntProperty(index: Int) = IntProperty(parts, index)
+	protected fun StringProperty(index: Int) = StringProperty(parts, index)
+	protected fun BooleanProperty(index: Int) = BooleanProperty(parts, index)
+	protected fun<T> ObjectProperty(index: Int, getter: (String) -> T, setter: (T) -> String) = ObjectProperty(parts, index, getter, setter)
+}
+
+
+
+abstract class Rome2Object2(line1: String, line2: String) {
+	protected val parts1 = line1.split('\t').toMutableList()
+	protected val parts2 = line2.split('\t').toMutableList()
+	val assembleLine1 get() = parts1.joinToString("\t")
+	val assembleLine2 get() = parts2.joinToString("\t")
+}
+
+
+
+/*
+Printing
+ */
+
+
+
 fun printUnits(vararg units: Unit) = units.forEach { it.printFormatted() }
 
 
@@ -16,7 +90,7 @@ fun Unit.printFormatted() {
 			morale:   $morale
 			bonusHp:  $bonusHp
 			charge:   $charge
-			armour:   $totalArmour = ${armour.value} ${armour.name} + ${shield.armour} ${shield.name}
+			armour:   $totalArmour = ${armour.armour} ${armour.name} + ${shield.armour} ${shield.name}
 			damage:   ${weapon.damage}  ${weapon.name}
 			bonus:    ${weapon.cavalryBonus}/${weapon.largeBonus}/${weapon.infantryBonus}
 			cost:     $cost
@@ -24,115 +98,6 @@ fun Unit.printFormatted() {
 	""".trimIndent())
 	println()
 }
-
-
-
-data class UnitInfo(
-	val name: String,
-	val cost: Int,
-	val upkeep: Int
-)
-
-
-
-data class Unit(
-	val name: String,
-	val attack: Int,
-	val defence: Int,
-	val morale: Int,
-	val bonusHp: Int,
-	val charge: Int,
-	val armour: Armour,
-	val weapon: Weapon,
-	val shield: Shield,
-	val cost: Int,
-	val upkeep: Int,
-) {
-	val totalArmour = armour.value + shield.armour
-	val totalDefence = defence + shield.defence
-	override fun hashCode() = name.hashCode()
-	override fun equals(other: Any?) = other is Unit && other.name == name
-}
-
-
-
-class IntProperty(private val parts: MutableList<String>, private val index: Int) {
-	private var value = parts[index].toInt()
-
-	operator fun getValue(thisRef: Any?, property: KProperty<*>) = value
-
-
-	operator fun setValue(thisRef: Any?, property: KProperty<*>, value: Int) {
-		parts[index] = value.toString()
-		this.value = value
-	}
-}
-
-
-
-class Armour2(line: String) {
-	private val parts = line.split('\t').toMutableList()
-	var armour by IntProperty(parts, 0)
-}
-
-
-
-data class Armour(
-	val name            : String,
-	val value           : Int,
-	val missileBonus    : Int,
-	val weakVsMissiles  : Boolean
-)
-
-
-
-data class Shield(
-	val name               : String,
-	val defence            : Int,
-	val armour             : Int,
-	val missileBlockChance : Int
-)
-
-
-
-data class Weapon(
-	val name              : String,
-	val damage            : Int,
-	val apDamage          : Int,
-	val cavalryBonus      : Int,
-	val largeBonus        : Int,
-	val infantryBonus     : Int,
-	val isArmourPiercing  : Boolean
-)
-
-
-
-data class BuildingEffect(
-	val effect       : String,
-	val scope        : String,
-	val value        : Int,
-	val valueDamaged : Int,
-	val valueRuined  : Int
-)
-
-
-
-data class BuildingInfo(
-	val name      : String,
-	val level     : Int,
-	val buildTime : Int,
-	val cost      : Int
-)
-
-
-
-data class Building(
-	val name     : String,
-	val level    : Int,
-	val turns    : Int,
-	val cost     : Int,
-	val effects  : List<BuildingEffect>
-)
 
 
 
@@ -144,4 +109,97 @@ fun Building.printFormatted() {
 		for(effect in effects)
 			append("\t${effect.effect} ${effect.scope} ${effect.value}\n")
 	})
+}
+
+
+
+/*
+Types
+ */
+
+
+
+/**
+ * land_units, main_units
+ */
+class Unit(line1: String, line2: String) : Rome2Object2(line1, line2) {
+	val name    by StringProperty(parts1, 0)
+	var attack  by IntProperty(parts1, 14)
+	var defence by IntProperty(parts1, 15)
+	var morale  by IntProperty(parts1, 16)
+	var bonusHp by IntProperty(parts1, 17)
+	var charge  by IntProperty(parts1, 6)
+	var armour  by ObjectProperty(parts1, 3, { armours[it]!! }, { it.name })
+	var weapon  by ObjectProperty(parts1, 22, { weapons[it]!! }, { it.name })
+	var shield  by ObjectProperty(parts1, 25, { shields[it]!! }, { it.name })
+	var cost    by IntProperty(parts2, 15)
+	var upkeep  by IntProperty(parts2, 18)
+
+	val totalDefence get() = defence + shield.defence
+	val totalArmour get() = armour.armour + shield.armour
+}
+
+
+
+/**
+ * unit_armour_types
+ */
+class Armour(line: String) : Rome2Object(line) {
+	val name            by StringProperty(parts, 0)
+	var armour          by IntProperty(parts, 1)
+	var bonusVsMissiles by IntProperty(parts, 2)
+	var weakVsMissiles  by BooleanProperty(parts, 3)
+}
+
+
+
+/**
+ * unit_shield_types
+ */
+class Shield(line: String) : Rome2Object(line) {
+	val name     by StringProperty(parts, 0)
+	var defence  by IntProperty(1)
+	var armour   by IntProperty(2)
+	var block    by IntProperty(4)
+}
+
+
+
+/**
+ * melee_weapons
+ */
+class Weapon(line: String) : Rome2Object(line) {
+	val name           by StringProperty(0)
+	var armourPiercing by BooleanProperty(2)
+	var cavalryBonus   by IntProperty(3)
+	var largeBonus     by IntProperty(4)
+	var infantryBonus  by IntProperty(5)
+	var damage         by IntProperty(6)
+	var apDamage       by IntProperty(7)
+}
+
+
+
+/**
+ * building_effects_junction
+ */
+class BuildingEffect(line: String) : Rome2Object(line) {
+	val effect       by StringProperty(1)
+	var scope        by StringProperty(2)
+	var value        by IntProperty(3)
+	var valueDamaged by IntProperty(4)
+	var valueRuined  by IntProperty(5)
+}
+
+
+
+/**
+ * building_levels
+ * building_effects_junction
+ */
+class Building(line: String, val effects: MutableList<BuildingEffect>) : Rome2Object(line) {
+	val name  by StringProperty(0)
+	val level by IntProperty(2)
+	val turns by IntProperty(4)
+	val cost  by IntProperty(5)
 }
